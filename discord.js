902aecdb9910f8d303b66c0f58ac43efdfb9b33c
yuta0801 <yuta20030801@icloud.com>
2017-11-30 22:15:31 +0900

    ユーザーログイン機能を実装

---
+++ discord.js
@@ -0,0 +1,50 @@
+const request = require('request')
+const config = require('./config.json')
+
+module.exports = {
+  login: (req, res) => {
+    res.redirect('https://discordapp.com/oauth2/authorize'+
+      `?response_type=code&client_id=${config.client.id}&scope=identify`+
+      `&redirect_uri=${encodeURIComponent(config.client.callback)}`)
+  },
+  logout: (req, res) => {
+    req.session.destroy()
+    res.redirect('/')
+  },
+  callback: (req, res) => {
+    getToken(req.query.code, data => {
+      request.get({
+        url: 'https://discordapp.com/api/v6/users/@me',
+        headers: {
+          'Authorization': 'Bearer ' + data.access_token,
+        },
+        json: true,
+      }, (err, response, data) => {
+        if (err) console.log(err)
+        req.session.user = data
+        res.redirect('/')
+      })
+    })
+  },
+}
+
+function getToken(code, callback) {
+  request({
+    url: 'https://discordapp.com/api/v6/oauth2/token',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    formData: {
+      client_id: config.client.id,
+      client_secret: config.client.secret,
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: config.client.callback,
+    },
+    json: true,
+  }, (err, res, data) => {
+    if (err) console.log(err)
+    callback(data)
+  })
+}
