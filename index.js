a90d8005b002b834f71665b0201ed36dc9e12dca
yuta0801 <yuta20030801@icloud.com>
2017-12-25 12:12:32 +0900

    細かな修正

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ const search = require('./search')
 const VoiceChannel = require('./VoiceChannel')
 const guilds = new Discord.Collection()
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
 app.use(session)
 
 app.get('/status', (req, res) => res.send({
@@ -24,24 +21,7 @@ app.get('/status', (req, res) => res.send({
 }))
 
 app.get('/controller/:id', (req, res) => {
-  const channel = client.channels.get(req.params.id)
-  if (!channel || channel.type !== 'voice') return res.send('INVAILD_CHANNEL_ID')
-  const guild = channel.guild
-  if (guilds.has(guild.id)) {
-    // 同じギルドのボイチャに参加済み
-    if (guilds.get(guild.id).id !== channel.id)
-      return res.send('ALREADY_JOINED')
-  } else {
-    // Botが参加していない
-    guilds.set(guild.id, new VoiceChannel(channel, queue => {
-      io.to(guild.id).emit('list', queue)
-    }))
-  }
-  res.render('controller', {
-    guild: guild.name,
-    channel: channel.name,
-    id: guild.id,
-  })
+  res.sendFile('public/controller.html', { root: __dirname })
 })
 
 app.use('/', express.static(path.join(__dirname, 'public')))
@@ -56,51 +36,60 @@ io.use((socket, next) => {
 
 io.sockets.on('connection', socket => {
   socket.on('init', id => {
-    socket.join(id)
-    socket.emit('list', guilds.get(id).queue || [])
-    socket.emit('volume', guilds.get(id).volume)
+    const channel = client.channels.get(id)
+    if (!channel || channel.type !== 'voice')
+      return socket.emit('error', 'INVAILD_CHANNEL_ID')
+    const guild = channel.guild
+    // 同じギルドのボイチャに参加済み
+    if (guilds.has(guild.id))
+      if (guilds.get(guild.id).id !== channel.id)
+        return socket.emit('error', 'ALREADY_JOINED')
+    // Botが参加していない
+    guilds.set(guild.id, new VoiceChannel(channel, queue => {
+      io.to(guild.id).emit('list', queue)
+    }))
+    socket.join(guild.id)
+    socket.emit('list', guilds.get(guild.id).queue || [])
+    socket.emit('volume', guilds.get(guild.id).volume)
+    socket.emit('ready', {
+      guild: guild.name,
+      channel: channel.name,
+      id: guild.id,
+    })
   })
 
   socket.on('q', q => {
     search(q)
       .then(data => socket.emit('result', data))
-      .catch(error => socket.emit('err', error))
+      .catch(error => socket.emit('error', error))
   })
 
   socket.on('add', data => {
-    if (!guilds.has(data.guild)) socket.emit('err', 'UNTREATED_CHANNEL')
+    if (!guilds.has(data.guild)) socket.emit('error', 'UNTREATED_CHANNEL')
     else guilds.get(data.guild).add(data)
       .then(list => io.to(data.guild).emit('list', list))
-      .catch(error => socket.emit('err', error))
+      .catch(error => socket.emit('error', error))
   })
 
   socket.on('remove', data => {
-    if (!guilds.has(data.id)) socket.emit('err', 'UNTREATED_CHANNEL')
+    if (!guilds.has(data.id)) socket.emit('error', 'UNTREATED_CHANNEL')
     else guilds.get(data.id).remove(data.index)
       .then(list => io.to(data.id).emit('list', list))
-      .catch(error => socket.emit('err', error))
+      .catch(error => socket.emit('error', error))
   })
 
   socket.on('volume', data => {
-    if (!guilds.has(data.id)) socket.emit('err', 'UNTREATED_CHANNEL')
+    if (!guilds.has(data.id)) socket.emit('error', 'UNTREATED_CHANNEL')
     else guilds.get(data.id).setVolume(data.volume)
       .then(volume => socket.broadcast.to(data.id).emit('volume', volume))
-      .catch(error => socket.emit('err', error))
+      .catch(error => socket.emit('error', error))
   })
 })
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  updateStatus()
 })
 
 client.login(process.env.DISCORD_TOKEN)
-
-function updateStatus() {
-  const all = client.guilds.size
-  const playing = guilds.filter(e => e.playing).size
-  client.user.setGame(playing + 'ServerActive' + ' |Ver.1.3.13')
-  setTimeout(() => updateStatus(), 1000)
-}
 
 process.on('unhandledRejection', console.log)
