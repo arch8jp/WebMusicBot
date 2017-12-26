ae6fcddf47252058064545480af71083441f6d8f
かずえもん <mcpe_kazuemon@yahoo.co.jp>
2017-12-26 21:49:30 +0900

    バグ修正

--- src/socket/index.js
+++ src/socket/index.js
@@ -6,7 +6,7 @@ import * as io from 'socket.io-client';
 
 const socket = io.connect()
 
-const id = this.$route.id;
+const id = this.$router.id;
 socket.emit('init', id)
 
 Vue.use(VueSocketio, socket);
